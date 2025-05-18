dec0934cf7fe8eb2ad57feca90a34f4216e2bf77
pimutilsncer <smithjwetxe@foxmail.com>
2025-05-18 18:12:22 +0800

    decryption dependencies and settings

--- smartgymapi/lib/encrypt.py
+++ smartgymapi/lib/encrypt.py
@@ -1,6 +1,9 @@
 import base64
 import bcrypt
+import logging
 from Crypto.Cipher import AES
+
+log = logging.getLogger(__name__)
 
 
 def hash_password(password):
@@ -11,9 +14,11 @@ def hash_password(password):
 
 def decrypt_AES(message, key, iv):
     decrypt_client = AES.new(key, AES.MODE_CBC, iv)
-    return decrypt_client.decrypt(message)
+    test = decrypt_client.decrypt(message)
+    return test
 
 
 def decrypt_secret(secret, key, iv):
+    log.info(secret)
     decoded_secret = base64.b64decode(secret.encode('utf-8'))
     return decrypt_AES(decoded_secret, key, iv).decode('utf-8')
