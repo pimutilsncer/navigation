e357af5472dbe35e127c93dc2fe7109bd8b87226
pimutilsncer <smithjwetxe@foxmail.com>
2026-02-02 08:58:23 +0800

    view config

--- smartgymapi/handlers/device.py
+++ smartgymapi/handlers/device.py
@@ -3,6 +3,7 @@ import logging
 
 from marshmallow import ValidationError
 from pyramid.httpexceptions import HTTPBadRequest, HTTPInternalServerError
+from pyramid.view import view_config, view_defaults
 
 from smartgymapi.lib.validation.device import DeviceSchema
 from smartgymapi.model import commit, persist, rollback, delete
@@ -11,10 +12,15 @@ from smartgymapi.models.device import Device
 log = logging.getLogger(__name__)
 
 
+@view_defaults(containment='smartgymapi.lib.factories.device.DeviceFactory',
+               permission='device',
+               renderer='json')
 class DeviceHandler(object):
     def __init__(self, request):
         self.request = request
 
+    @view_config(context=Device, request_method='POST', permission='checkin',
+                 name='checkin')
     def checkin(self):
         schema = DeviceSchema(strict=True)
         try:
@@ -27,12 +33,16 @@ class DeviceHandler(object):
 
         # launch activity code
 
+    @view_config(context='smartgymapi.lib.factories.device.DeviceFactory',
+                 request_method='GET')
     def list(self):
         return DeviceSchema(
             many=True,
             only=('name', 'device_address', 'device_class')
         ).dump(self.request.context.get_devices())
 
+    @view_config(context='smartgymapi.lib.factories.device.DeviceFactory',
+                 request_method='POST')
     def post(self):
         schema = DeviceSchema(strict=True, only=('name', 'device_address',
                                                  'device_Class'))
@@ -54,6 +64,7 @@ class DeviceHandler(object):
         finally:
             commit()
 
+    @view_config(context=Device, request_method='DELETE')
     def delete(self):
         try:
             delete(self.request.context)
